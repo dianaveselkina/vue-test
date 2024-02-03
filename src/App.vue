e72337873e5b7dca84a9ad8c87d07b7a0360230d
dianaveselkina <vess_di@mail.ru>
2024-02-03 14:12:24 +0300

    create project

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <div class="post">
+      <div><strong>Название:</strong>Пост о JavaScript</div>
+      <div>
+        <strong>Описание:</strong>JavaScript универсальный язык программирования
+      </div>
+    </div>
+    <div class="post">
+      <div><strong>Название:</strong>Пост о JavaScript</div>
+      <div>
+        <strong>Описание:</strong>JavaScript универсальный язык программирования
+      </div>
+    </div>
+    <div class="post">
+      <div><strong>Название:</strong>Пост о JavaScript</div>
+      <div>
+        <strong>Описание:</strong>JavaScript универсальный язык программирования
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      likes: 0,
+      dislikes: 5,
+    };
+  },
+  methods: {
+    addLike() {
+      this.likes += 1;
+    },
+    addDisLike() {
+      this.dislikes += 1;
+    },
+  },
+};
+</script>
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+.post {
+  padding: 15px;
+  border: 2px solid teal;
+  margin-top: 15px;
+}
+</style>
