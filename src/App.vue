030f46c207c5f97d511e63ce450299912aeb222a
dianaveselkina <vess_di@mail.ru>
2024-02-03 22:54:56 +0300

    add components

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,17 @@
 <template>
   <div class="app">
-    <h4>Создание поста</h4>
-    <form @submit.prevent>
-      <input
-        v-bind:value="title"
-        @input="title = $event.target.value"
-        class="input"
-        type="text"
-        placeholder="Название"
-      />
-      <input
-        v-bind:value="body"
-        @input="body = $event.target.value"
-        class="input"
-        type="text"
-        placeholder="Описание"
-      />
-      <button class="btn" @click="createPost">Создать</button>
-    </form>
-    <div class="post" v-for="post in posts">
-      <div><strong>Название:</strong>{{ post.title }}</div>
-      <div><strong>Описание:</strong>{{ post.body }}</div>
-    </div>
+    <post-form />
+    <post-list />
   </div>
 </template>
 <script>
+import PostList from '@/components/PostList.vue';
+import PostForm from '@/components/PostForm.vue';
 export default {
+  components: {
+    PostList,
+    PostForm,
+  },
   data() {
     return {
       posts: [
@@ -45,6 +31,7 @@ export default {
         body: this.body,
       };
       this.posts.push(newPost);
+      (this.title = ''), (this.body = '');
     },
   },
 };
@@ -57,28 +44,5 @@ export default {
 }
 .app {
   padding: 20px;
-}
-form {
-  display: flex;
-  flex-direction: column;
-}
-.post {
-  padding: 15px;
-  border: 2px solid teal;
-  margin-top: 15px;
-}
-.input {
-  width: 100%;
-  border: 1px solid teal;
-  padding: 10px 15px;
-  margin-top: 15px;
-}
-.btn {
-  margin-top: 15px;
-  align-self: flex-end;
-  padding: 10px 15px;
-  background: none;
-  color: teal;
-  border: 1px solid teal;
 }
 </style>
---
+++ src/components/PostList.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div class="post" v-for="post in posts">
+      <div><strong>Название:</strong>{{ post.title }}</div>
+      <div><strong>Описание:</strong>{{ post.body }}</div>
+    </div>
+  </div>
+</template>
+<script></script>
+<style scoped>
+.post {
+  padding: 15px;
+  border: 2px solid teal;
+  margin-top: 15px;
+}
+</style>
