8a26944e9f86df17d65039f6b03c78bf8a4e1999
dianaveselkina <vess_di@mail.ru>
2024-02-04 18:49:05 +0300

    add MyButton

--- src/components/PostItem.vue
+++ src/components/PostItem.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="post">
-    <div><strong>Название:</strong>{{ post.title }}</div>
-    <div><strong>Описание:</strong>{{ post.body }}</div>
+    <div>
+      <div><strong>Название:</strong>{{ post.title }}</div>
+      <div><strong>Описание:</strong>{{ post.body }}</div>
+    </div>
+    <div class="post__btn">
+      <button>Удалить</button>
+    </div>
   </div>
 </template>
 <script>
@@ -14,4 +19,13 @@ export default {
   },
 };
 </script>
-<style scoped></style>
+<style scoped>
+.post {
+  padding: 15px;
+  border: 2px solid teal;
+  margin-top: 15px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+</style>
