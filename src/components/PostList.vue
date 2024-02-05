649052471961e84375b41cbca1ccb59b9e108395
dianaveselkina <vess_di@mail.ru>
2024-02-05 13:11:31 +0300

    delete post

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,8 +1,14 @@
 <template>
-  <div>
-    <h3>Список пользователей</h3>
-    <post-item v-for="post in posts" :post="post" />
+  <div v-if="posts.length > 0">
+    <h3>Список постов</h3>
+    <post-item
+      v-for="post in posts"
+      :post="post"
+      :key="post.id"
+      @remove="$emit('remove', post)"
+    />
   </div>
+  <h2 v-else style="color: red">Список постов пуст</h2>
 </template>
 <script>
 import PostItem from '@/components/PostItem';
