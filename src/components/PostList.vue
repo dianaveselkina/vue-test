6986d79cf3d39f113164aa27dc47003f1bc88468
dianaveselkina <vess_di@mail.ru>
2024-02-06 00:36:13 +0300

    change animation

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -31,11 +31,14 @@ export default {
 }
 .post-list-enter-active,
 .post-list-leave-active {
-  transition: all 1s ease;
+  transition: all 0.4s ease;
 }
 .post-list-enter-from,
 .post-list-leave-to {
   opacity: 0;
-  transform: translateY(30px);
+  transform: translateX(130px);
+}
+.post-list-move {
+  transition: transform 0.4s ease;
 }
 </style>
